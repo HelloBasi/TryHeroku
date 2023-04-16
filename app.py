50a037dc3bb6983f687a541b45e4423eb792fc83
Binbasri-in <mohammed.binbasri@gmail.com>
2023-04-16 06:04:16 +0530

    added database and added helpers functions

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
 
-
+from helpers import apology, user_login_required, collector_login_required
 
 app = Flask(__name__)
 
@@ -14,6 +19,15 @@ def after_request(response):
     response.headers["Expires"] = 0
     response.headers["Pragma"] = "no-cache"
     return response
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///Recycle.db")
 
 @app.route('/')
 def index():
